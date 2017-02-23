fb95a1d1668354c861dee539cb226261cd878cc5
Lukas Pessl <lukaspessl@gmx.net>
2017-02-23 15:41:20 +0100

    update

--- environments/production/manifests/config.pp
+++ environments/production/manifests/config.pp
@@ -7,12 +7,12 @@ class { 'vmbuilder':
 
 	# General Kit Information
 	bibboxkit		=> "eB3Kit",
-	bibboxbaseurl	=> "eb3kit.bibbox.org",
+	bibboxbaseurl		=> "eb3kit.bibbox.org",
 	serveradmin		=> "admin@bibbox.org",
 
 	# Database Information
 	db_user			=> "liferay",
-	db_password		=> "CHANGEulHbbFpulHbM74JuBk9@CwMS",
+	db_password		=> "bibbox4ever",
 	db_name			=> "lportal"
 
 }
