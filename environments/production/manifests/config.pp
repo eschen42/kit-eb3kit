0423a962628dd88e0e0560b1fa19f1f121b3d29a
Lukas Pessl <lukaspessl@gmx.net>
2017-03-09 10:41:58 +0100

    availability warning

--- environments/production/manifests/config.pp
+++ environments/production/manifests/config.pp
@@ -6,6 +6,7 @@
 class { 'vmbuilder':
 
 	# General Kit Information
+	# Currently only "eB3Kit" is available for bibboxkit
 	bibboxkit		=> "eB3Kit",
 	bibboxbaseurl		=> "eb3kit.bibbox.org",
 	serveradmin		=> "admin@bibbox.org",
