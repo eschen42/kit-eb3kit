0f0825fcda4fd96217e246667f87aa981e393c2e
lomadi <lomadi@mac.com>
2018-02-02 19:41:15 +0100

    allow for local liferay file (to be tested)

--- download-liferay.sh
+++ download-liferay.sh
@@ -3,8 +3,13 @@
 echo "pre-download liferay"
 
 if [ ! -f "/opt/liferay-ce-portal-tomcat-7.0-ga3.zip" ]; then
-      echo "Downloading Liferay, this might take a while...";
-      wget -nc -nv -P /opt/ "http://downloads.bibbox.org/liferay-ce-portal-tomcat-7.0-ga3.zip";
-    else
-      echo "Liferay sources already exist. Skipping download.";
+    if [ ! -f "/vagrant/liferay-ce-portal-tomcat-7.0-ga3.zip" ]; then
+            echo "Copying Liferay,";
+            cp /vagrant/liferay-ce-portal-tomcat-7.0-ga3.zip /opt/
+        else
+            echo "Downloading Liferay, this might take a while...";
+            wget -nc -nv -P /opt/ "http://downloads.bibbox.org/liferay-ce-portal-tomcat-7.0-ga3.zip";
+        fi        
+else
+  echo "Liferay sources already exist";    
 fi
