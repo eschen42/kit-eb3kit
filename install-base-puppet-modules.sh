12c49abae48c5895691abdf5b81159f7636dffb8
reihsr <robert.reihs@gmail.com>
2018-02-05 09:51:39 +0100

    Update install-base-puppet-modules.sh

--- install-base-puppet-modules.sh
+++ install-base-puppet-modules.sh
@@ -2,6 +2,12 @@
 
 echo "install puppet modules"
 
+if [ $? ]; then
+  export PATH=/opt/puppetlabs/bin:$PATH
+  echo "new PATH: $PATH"
+fi
+
+
 puppet module install puppetlabs-stdlib --version 4.14.0  --modulepath /etc/puppetlabs/code/modules
 puppet module install puppetlabs-apt --version 2.3.0      --modulepath /etc/puppetlabs/code/modules
 puppet module install puppetlabs-ntp --version 6.0.0      --modulepath /etc/puppetlabs/code/modules
@@ -17,4 +23,4 @@ puppet module install tylerwalts-jdk_oracle --version 2.0.0 --modulepath /etc/pu
 # finaly copy our modules to the /etc/puppetlabs/code/modules
 #
 echo "copy BIBBOX puppet modules"
-cp -r /vagrant/modules/* /etc/puppetlabs/code/modules
+cp -r modules/* /etc/puppetlabs/code/modules
