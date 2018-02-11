f3b7854d8965d7acd691c7093c32bb1b96ca9b04
Robert Reihs <robert.reihs@medunigraz.at>
2018-02-11 08:41:11 +0100

    Adding VM Import Script

---
+++ import_basis_image_bibbox.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+echo "Import BIBBOX Basis OVA"
+
+vmname_command=""
+vmname="BIBBOX-v-2-0-2"
+ova="bibbox-v-2-0-2_ubuntu_basis.ova"
+p80="80"
+p22="22"
+
+import() {
+	VBoxManage import $ova $vmname_command
+}
+
+setport() {
+	VBoxManage modifyvm $vmname --natpf1 "ssh,tcp,127.0.0.1,${p22},,22"
+	VBoxManage modifyvm $vmname --natpf1 "http,tcp,127.0.0.1,${p80},,80"
+}
+
+startvm() {
+	VBoxManage startvm $vmname --type headless
+}
+
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -vn | --vmname )        shift
+								vmname=$1
+                                vmname_command="--vsys 0 --vmname $vmname"
+                                ;;
+        -ova | --ovafile )      shift
+                                ova=$1
+                                ;;
+        -ssh | --ssh22 )        shift
+                                p22=$1
+                                ;;
+        -http | --http80 )      shift
+                                p80=$1
+                                ;;
+        * )                     usage
+                                error_exit "Parameters not matching"
+    esac
+    shift
+done
+
+import()
+setport()
+startvm()
