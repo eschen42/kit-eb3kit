622f004f6767e27ec966f043214ad2cb0d84d329
Robert Reihs <robert.reihs@medunigraz.at>
2018-02-15 19:38:15 +0100

    GUI installer and command properties

--- install-bibbox-server.sh
+++ install-bibbox-server.sh
@@ -6,10 +6,28 @@
 
 echo " --- Installing BIBBOX Server"
 
+#---------------------------------
+url=lab.box
+gui=false
+while [ "$1" != "" ]; do
+    case $1 in
+        -url | --base-url )     shift
+								url=$1
+                                ;;
+        -gui | --install-gui )  gui=true
+                                ;;
+        * )                     usage
+                                error_exit "Parameters not matching"
+    esac
+    shift
+done
+
+#---------------------------------
+echo "Install GIT"
 apt-get install -y git
 mkdir /vagrant/modules/
 
-
+#---------------------------------
 DIRECTORY="/vagrant"
 if [ ! -d "$DIRECTORY" ]; then
   echo " --- Coning KIT Repository"
@@ -18,9 +36,11 @@ else
   echo " --- Update Kit git Repository"
   git -C /vagrant/ pull
 fi
-
 chmod +x /vagrant/*.sh
 
+sed -i "s/eb3kit.bibbox.org/$url/g" /vagrant/environments/production/manifests/config.pp
+ 
+#---------------------------------
 echo " --- Install Python and Tools"
 /vagrant/install-pyhthon-and-tools.sh
 echo " --- download Liferay"
@@ -30,9 +50,15 @@ echo " --- Install Puppet"
 echo " --- Install Puppet modules"
 /vagrant/install-base-puppet-modules.sh
 
+#---------------------------------
 echo " --- Run Puppet"
 if [ $? ]; then
   export PATH=/opt/puppetlabs/bin:$PATH
   echo "new PATH: $PATH"
 fi
 puppet apply --modulepath=/etc/puppetlabs/code/modules -L /vagrant/environments/production/manifests/config.pp /vagrant/modules/vmbuilder/manifests/init.pp
+
+if [ "$gui" = true ] ; then
+    echo " --- Install GUI"
+	/vagrant/install-gui.sh
+fi
--- install-gui.sh
+++ install-gui.sh
@@ -5,3 +5,8 @@ echo "Install GUI"
 apt-get install -y ubuntu-desktop
 apt-get install -y dnsmasq
 
+sed -i "s/enabled=1/enabled=0/g" /etc/default/apport
+service apport restart
+
+# /etc/dnsmasq.conf address=/*.$url/???.???.???.???
+
