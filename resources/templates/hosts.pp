061d036369e021928ea53f7a845c4c76f1e2c1b6
Heimo Müller <lomadi@mac.com>
2018-10-15 08:16:51 +0200

    Update hosts.pp

--- resources/templates/hosts.pp
+++ resources/templates/hosts.pp
@@ -3,6 +3,8 @@
 127.0.0.1       <%= $subdomain %>
 127.0.0.1       localhost
 127.0.1.1       localhost.vm    localhost
+127.0.1.1       bibbox.local.domain
+
 ::1     localhost ip6-localhost ip6-loopback
 ff02::1 ip6-allnodes
 ff02::2 ip6-allrouters
