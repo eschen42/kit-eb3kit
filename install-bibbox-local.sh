ffa930f8157c6df6ef61e70f5143daacfb6a90c4
lomadi <lomadi@mac.com>
2018-10-15 22:16:42 +0200

    Update install-bibbox-local.sh

--- install-bibbox-local.sh
+++ install-bibbox-local.sh
@@ -61,6 +61,7 @@ if [ "$gui" = true ] ; then
     service apport restart
 
     sed -i "s|#conf-dir=/etc/dnsmasq.d/,\*.conf|conf-dir=/etc/dnsmasq.d/,*.conf|g" /etc/dnsmasq.conf
+    sed -i '1i127.0.0.1 $url' /etc/hosts
+    
     echo "address=/*.$url/127.0.0.1" > /etc/dnsmasq.d/bibbox-local.conf
-
 fi
