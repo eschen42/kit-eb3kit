ce8c514f0c7345063cb04247ce990e9273b01b41
lomadi <lomadi@mac.com>
2018-10-15 21:34:08 +0200

    Update install-bibbox-local.sh

--- install-bibbox-local.sh
+++ install-bibbox-local.sh
@@ -54,5 +54,13 @@ sed -i "s/eb3kit.bibbox.org/$url/g"  /opt/bibbox-install/environments/local/mani
 
 
 if [ "$gui" = true ] ; then
-	/opt/bibbox-install/install-gui.sh
+    apt-get install -y ubuntu-desktop
+    apt-get install -y dnsmasq
+
+    sed -i "s/enabled=1/enabled=0/g" /etc/default/apport
+    service apport restart
+
+    sed -i "s|#conf-dir=/etc/dnsmasq.d/,\*.conf|conf-dir=/etc/dnsmasq.d/,*.conf|g" /etc/dnsmasq.conf
+    echo "address=/*.$url/127.0.0.1" > /etc/dnsmasq.d/bibbox-local.conf
+
 fi
